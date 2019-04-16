32accd10bdf5ff8e7a98578f55773f19b8195340
EduCowboy <marcmarc2003@gmail.com>
2019-04-15 21:21:41 -0300

    User Authetication Commit 1

---
+++ DatingApp.API/Services/UserServices.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace DatingApp.API.Services
+{
+    public class UserServices
+    {
+        
+    }
+}
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using DatingApp.API.Data.Interfaces;
+using DatingApp.API.Data.Repository;
 
 namespace DatingApp.API
 {
@@ -30,6 +32,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
